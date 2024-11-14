66a384d648f1ffcf1b9f9b434c51236629e4cda1
tom <tom@jadecross.com>
2024-11-14 22:00:49 +0900

    [UPDATE] Chagen TfState Backend

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
-  // cloud {
-  //   organization = "<MY_ORG_NAME>"         # 생성한 ORG 이름 지정
-  //   hostname     = "app.terraform.io"      # default
+  cloud {
+    organization = "yu3papa-org"         # 생성한 ORG 이름 지정
+    hostname     = "app.terraform.io"      # default
 
-  //   workspaces {
-  //     name = "terraform-aws-collaboration" # 없으면 생성됨
-  //   }
-  // }
+    workspaces {
+      name = "terraform-aws-collaboration" # 없으면 생성됨
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
